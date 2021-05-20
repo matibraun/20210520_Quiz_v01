585452ea165b44071a198db7d38fbc7fd4032bbf
matibraun <matibraun@gmail.com>
2021-05-20 18:34:44 +0200

    Finalizada

--- quizbee/src/index.js
+++ quizbee/src/index.js
@@ -2,12 +2,16 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import "./assets/style.css";
-import quizService from "./quizService"
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
+import Result from "./components/Result";
 
 class QuizBee extends Component {
 
     state = {
-        questionBank: []
+        questionBank: [],
+        score: 0,
+        responses: 0,
     };
 
     getQuestions = () => {
@@ -17,6 +21,26 @@ class QuizBee extends Component {
             });
         });
     };
+
+    computeAnswer = (answer, correctAnswer) => {
+        if (answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            })
+        }
+
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+        })
+    }
+
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({
+            score: 0,
+            responses: 0,
+        });
+    }
 
     componentDidMount() {
         this.getQuestions();
@@ -28,9 +52,20 @@ class QuizBee extends Component {
             <div className="container">
                 <div className="title">QuizBee</div>
                 {this.state.questionBank.length > 0 && 
+                    this.state.responses < 5 &&
                     this.state.questionBank.map(
-                        ({question, answers, correct, questionId}) => <h4>{question}</h4>
-                )}
+                        ({question, answers, correct, questionId}) => (
+                            <QuestionBox
+                                question={question}
+                                options={answers}
+                                key={questionId}
+                                selected={answer => this.computeAnswer(answer, correct)}
+                            />
+                        )
+                    )
+                }
+
+                {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain} />) : null}
             </div>
         );
     };
