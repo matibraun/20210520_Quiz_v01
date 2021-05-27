11925c9e9fc758e3519f8909223e6331122e1107
matibraun <matibraun@gmail.com>
2021-05-27 12:54:39 +0200

    probando

--- quizbee/src/components/QuestionBox.js
+++ quizbee/src/components/QuestionBox.js
@@ -1,13 +1,26 @@
 import React, {useState} from "react";
 
-const QuestionBox = ({question, options, selected}) => {
+const QuestionBox = ({question, options, correctAnwer}) => {
     
     const [answer, setAnswer] = useState(null);
+    
+    if (answer === null) {
 
-    return (
-        <div className="questionBox">
-
-            <div className="question">{question}</div>
+        return (
+            <div className="questionBox">
+    
+                <div className="question">{question}</div>
+    
+                {options}.map((text, index) => (
+                <button key={index} className="answerBtn" onClick={() => {
+                    setAnswer(text);
+                }}>
+                    {text}
+                </button>
+                ))
+            
+            </div>
+    }
 
             {answer
             
@@ -18,17 +31,8 @@ const QuestionBox = ({question, options, selected}) => {
             </div>
             
             :
-            options.map((text, index) => (
-                <button key={index} className="answerBtn" onClick={() => {
-                    setAnswer(text);
-                    selected(text);
-                }}>
-                    {text}
-                </button>
-            ))
-            }
 
-        </div>
+        
     )
 }
 
